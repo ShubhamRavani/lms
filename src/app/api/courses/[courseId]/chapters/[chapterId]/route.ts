f4a3b424df7dcb434504ddecdd763b15b2f73bc7
ShubhamRavani <shubhamravani67890@gmail.com>
2024-02-28 16:55:49 +0530

    feat(chapters): add mux to manage video data

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+import Mux from '@mux/mux-node'
 import { db } from '@/lib/db'
+
+const { video } = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+})
 
 export async function PATCH(req: Request, { params }: { params: { chapterId: string; courseId: string } }) {
   try {
@@ -18,6 +24,29 @@ export async function PATCH(req: Request, { params }: { params: { chapterId: str
     }
 
     const chapter = await db.chapter.update({ where: { id: params.chapterId }, data: { ...values } })
+
+    if (values.videoUrl) {
+      /** Cleaning up existing data */
+      const existingMuxData = await db.muxData.findFirst({ where: { chapterId: params.chapterId } })
+      if (existingMuxData) {
+        await video.assets.delete(existingMuxData.assetId)
+        await db.muxData.delete({ where: { id: existingMuxData.id } })
+      }
+
+      const asset = await video.assets.create({
+        input: values.videoUrl,
+        playback_policy: ['public'],
+        test: false,
+      })
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      })
+    }
 
     return NextResponse.json(chapter)
   } catch {
