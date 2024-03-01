be0f105e5e8d11a2aa2fb7ef4dae959a7a9cdfbf
ShubhamRavani <shubhamravani67890@gmail.com>
2024-03-01 16:16:07 +0530

    feat(courses): add course sidebar for user's view

--- src/components/navbar-routes/navbar-routes.tsx
+++ src/components/navbar-routes/navbar-routes.tsx
@@ -11,7 +11,7 @@ export default function NavbarRoutes() {
   const pathname = usePathname()
 
   const isTeacherPage = pathname?.startsWith('/teacher')
-  const isPlayerPage = pathname?.includes('/chapter')
+  const isPlayerPage = pathname?.includes('/courses')
   const isSearchPage = pathname === '/search'
 
   return (
